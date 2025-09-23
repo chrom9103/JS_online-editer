a7e9381ce192e0b6eddc7bd1e6be7b8e300d662f
chrom9103 <kdkd21347.tm@gmail.com>
2025-09-23 22:30:30 +0900

    Refactor: Remove unused components and consolidate views
    - Deleted IconSupport.vue and IconTooling.vue as they are no longer needed.
    - Removed Page.vue and its associated styles, consolidating project display logic into MainView.vue.
    - Eliminated AboutView.vue and HomeView.vue, integrating their functionality into MainView.vue.
    - Updated router to point to MainView.vue for the home route.
    - Removed WorksView.vue and its dependencies, streamlining the project structure.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,22 +6,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
-      path: '/works',
-      name: 'works',
-      component: () => import('../views/works/WorksView.vue'),
-    },
-    {
-      path: '/works/playground',
-      name: 'playground',
-      component: () => import('../views/works/PlayGround/MainView.vue'),
+      component: () => import('../views/MainView.vue'),
     },
   ],
 })
