9adb2b9674804620caece2d1075d3ac3d804cd24
chrom9103 <kdkd21347.tm@gmail.com>
2025-10-10 18:21:00 +0900

    fix(vite): correct base path in configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/woreks/playground',
+  base: '/works/playground/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
